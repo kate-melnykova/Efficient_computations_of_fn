a9e64e0fc4c88c61283f98e16914bb79919f1115
Kateryna Melnykova <forkatemelnikova@gmail.com>
2019-07-12 15:00:48 -0700

    Added view results button

--- flask_interface.py
+++ flask_interface.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -19,43 +17,58 @@ ID = 0
 def implementation():
     global ID, tables
     if request.method == 'POST':
-        func_name = request.form["func_name"]
-        inp_val = int(request.form['inp'])
-        n_digits = int(request.form['n_digits'])
+        if request.form['submit_button'] == 'submit':
+            func_name = request.form["func_name"]
+            inp_val = int(request.form['inp'])
+            n_digits = int(request.form['n_digits'])
 
-        tables.append([ID, func_name, inp_val, n_digits, "Computing...", "", ""])
-        if len(tables) > 10:
-            del tables[0]
-        id_loc = ID
-        ID += 1
+            tables.append([ID, func_name, inp_val, n_digits, "Computing...", "", ""])
+            if len(tables) > 10:
+                del tables[0]
+            id_loc = ID
+            ID += 1
 
-        [out_val, accuracy] = registry[func_name](inp_val, n_digits)
-        out_val = str(out_val)
-        tables[-1][4] = "yes"
+            [out_val, accuracy] = registry[func_name](inp_val, n_digits)
+            out_val = str(out_val)
+            tables[-1][4] = "yes"
 
-        # crop number if needed
-        if len(out_val) > n_digits:
-            if "." not in out_val[:n_digits]:
-                out_val = out_val[:n_digits+1] + "E+" + str(len(out_val)-n_digits)
-            else:
-                out_val = out_val[:n_digits + 2]
+            # crop number if needed
+            if len(out_val) > n_digits:
+                if "." not in out_val[:n_digits]:
+                    out_val = out_val[:n_digits+1] + "E+" + str(len(out_val)-n_digits)
+                else:
+                    out_val = out_val[:n_digits + 2]
 
-        idx = 70
-        while idx < len(out_val):
-            out_val = out_val[:idx] + '\n ...' + out_val[idx:]
-            idx += 70
+            idx = 70
+            while idx < len(out_val):
+                out_val = out_val[:idx] + '\n ...' + out_val[idx:]
+                idx += 70
 
-        #find row with ID = id_loc
-        for i in range(len(tables)):
-            found = tables[i][0] == id_loc
+            #find row with ID = id_loc
+            for i in range(len(tables)):
+                found = tables[i][0] == id_loc
+                if found:
+                    break
             if found:
-                break
-        if found:
-            tables[i][5] = out_val
-            tables[i][6] = accuracy
+                tables[i][5] = out_val
+                tables[i][6] = accuracy
 
-        return render_template('webpage.html', tables=tables,
-                               inp=inp_val, func_name=func_name, out_val=out_val, acc=accuracy)
+            return render_template('webpage.html', tables=tables,
+                                   inp=inp_val, func_name=func_name, out_val=out_val, acc=accuracy)
+        else:
+            # we need to figure out which form is submitted
+            id_view = request.form['submit_button']
+            for i in range(len(tables)):
+                found = tables[i][0] == int(id_view)
+                if found:
+                    break
+            if found:
+                return render_template('webpage.html', tables=tables,
+                                       inp=tables[i][2], func_name=tables[i][1],
+                                       out_val=tables[i][5], acc=tables[i][6])
+            else:
+                return render_template('webpage.html', tables=tables, inp=None, func_name=None,
+                                       out_val=None, acc=None)
     else:
         return render_template('webpage.html', tables=tables,
                                inp=None, func_name=None, out_val=None, acc=None)
