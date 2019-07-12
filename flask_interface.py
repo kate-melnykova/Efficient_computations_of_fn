e719d78b421b441a82c43dd9792dda6c8b773530
Kateryna Melnykova <forkatemelnikova@gmail.com>
2019-07-12 14:10:01 -0700

    Updated computation table

--- flask_interface.py
+++ flask_interface.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from flask_table import Table, Col
+
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -10,52 +10,30 @@ from compute_e import compute_e
 
 app = Flask(__name__)
 
-
-# Declare your table
-class ItemTable(Table):
-    Id = Col('ID')
-    func_name = Col('Function name')
-    inp = Col('Argument')
-    n_digits = Col('Number of digits of interest')
-    view = Col('Completed?')
-
-
-# Get some objects
-class Item:
-    def __init__(self, info):
-        self.Id = info[0]
-        self.func_name = info[1]
-        self.inp = info[2]
-        self.n_digits = info[3]
-        self.view = info[4]
-
-
-registry = {'factorial':factorial, 'pi':compute_pi, 'e':compute_e}
-computed = pd.DataFrame(columns=["ID", "func_name", "inp", "n_digits", "computed?", "out_val", "accuracy"])
-tables = ItemTable([Item(row) for row in computed.values.tolist()])
+registry = {'factorial': factorial, 'pi': compute_pi, 'e': compute_e}
+tables = []
 ID = 0
 
 
-@app.route("/", methods=["POST","GET"])
+@app.route("/", methods=["POST", "GET"])
 def implementation():
-    global ID, computed, tables
+    global ID, tables
     if request.method == 'POST':
         func_name = request.form["func_name"]
         inp_val = int(request.form['inp'])
         n_digits = int(request.form['n_digits'])
 
-        computed.loc[computed.shape[0]] = [ID, func_name, inp_val, n_digits, "In progress", "", ""]
-        print(computed)
-        if computed.shape[0] > 10:
-            computed = computed.iloc[1 :: ].reset_index(drop=True)
+        tables.append([ID, func_name, inp_val, n_digits, "Computing...", "", ""])
+        if len(tables) > 10:
+            del tables[0]
+        id_loc = ID
         ID += 1
-        tables = ItemTable([Item(row) for row in computed.values.tolist()])
 
         [out_val, accuracy] = registry[func_name](inp_val, n_digits)
         out_val = str(out_val)
-        computed.loc[computed.shape[0]-1, "computed?"] = "yes"
+        tables[-1][4] = "yes"
 
-        #crop number if needed
+        # crop number if needed
         if len(out_val) > n_digits:
             if "." not in out_val[:n_digits]:
                 out_val = out_val[:n_digits+1] + "E+" + str(len(out_val)-n_digits)
@@ -67,10 +45,14 @@ def implementation():
             out_val = out_val[:idx] + '\n ...' + out_val[idx:]
             idx += 70
 
-        computed.iloc[-1, 5] = out_val
-        computed.iloc[-1, 6] = accuracy
-        tables = ItemTable([Item(row) for row in computed.values.tolist()])
-
+        #find row with ID = id_loc
+        for i in range(len(tables)):
+            found = tables[i][0] == id_loc
+            if found:
+                break
+        if found:
+            tables[i][5] = out_val
+            tables[i][6] = accuracy
 
         return render_template('webpage.html', tables=tables,
                                inp=inp_val, func_name=func_name, out_val=out_val, acc=accuracy)
@@ -79,8 +61,5 @@ def implementation():
                                inp=None, func_name=None, out_val=None, acc=None)
 
 
-
 if __name__ == '__main__':
-    #execfile('modbusreginfo.py')
-    app.run(host='localhost', port=5000, debug=True)#, threaded=True)
-    #app.run()
+    app.run(host='localhost', port=5000, debug=True)
